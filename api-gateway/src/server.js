56f2e4218b4f8d6a3964e6cd57f6f84824ad1e8d
Tanmay <pasalkar8899@gmail.com>
2026-02-04 22:06:45 +0530

    fix: support mongo url in railway

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const app = require("./app");
 
-app.listen(process.env.PORT, () => {
-  console.log(`API Gateway running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`API Gateway running on port ${PORT}`);
 });
